5a1e142c64ea600476574662d0477dbaa61c4376
VISHNU <VE9986@SRMIST.EDU.IN>
2026-03-10 14:32:20 +0530

    UC3:hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class UseCase3PalindromeCheckerApp {
+
+    // Method to reverse a string using a for loop
+    public static String reverseString(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i); // String concatenation
+        }
+        return reversed;
+    }
+
+    // Method to check if a string is a palindrome using string reverse
+    public static boolean isPalindrome(String str) {
+        String reversed = reverseString(str);
+        return str.equals(reversed); // Using equals() to compare content
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("========================================");
+        System.out.println("  UC3: Palindrome Check - String Reverse");
+        System.out.println("========================================");
+        System.out.print("Enter a string to check: ");
+        String input = scanner.nextLine();
+
+        // Reverse the string
+        String reversed = reverseString(input);
+        System.out.println("Original String : " + input);
+        System.out.println("Reversed String : " + reversed);
+
+        // Compare and display result
+        if (isPalindrome(input)) {
+            System.out.println("Result: \"" + input + "\" IS a palindrome.");
+        } else {
+            System.out.println("Result: \"" + input + "\" is NOT a palindrome.");
+        }
+
+        scanner.close();
     }
 }
